e05ec1e81fd9abe6374b6aa7c901434ef17acf6e
Ismael <ismael.em@outlook.com>
2020-09-14 18:40:29 +0300

    packaging-graph: BuGn colors

--- vis/packaging/packaging-graph.py
+++ vis/packaging/packaging-graph.py
@@ -10,7 +10,8 @@ from plotly.subplots import make_subplots
 
 # #############################################################################
 # Read and clean the dataset
-df = pd.read_csv("C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\vis\\packaging\\packaging-waste-recycling.csv")
+df = pd.read_csv(
+    "C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\vis\\packaging\\packaging-waste-recycling.csv")
 
 df.drop(columns=["target_2008", "target_2025", "target_2030"], inplace=True)
 df.rename(columns={"generated_waste": "Generated",
@@ -36,16 +37,16 @@ def human_format(num):
         num /= 1000.0
     # add more suffixes if you need them
     n = ("%.3f" % num).strip("0").strip(".")
-    return "%s %s" % (n, [' kg', ' Ton', ' KTon'][magnitude]) 
+    return "%s %s" % (n, [' kg', ' Ton', ' KTon'][magnitude])
 
 
 def readable_format(num):
     return "{:.2f}".format(num / 1000).strip("0").strip(".") + " Ton"
 
 
-df["Generated"] = df["Generated"].apply(lambda x: readable_format(x) )
-df["Recycled"] = df["Recycled"].apply(lambda x: readable_format(x) )
-df["Share"] = df["Share"].apply(lambda x: str(x) + "%" )
+df["Generated"] = df["Generated"].apply(lambda x: readable_format(x))
+df["Recycled"] = df["Recycled"].apply(lambda x: readable_format(x))
+df["Share"] = df["Share"].apply(lambda x: str(x) + "%")
 
 # #############################################################################
 # Map + table
@@ -54,18 +55,20 @@ fig = make_subplots(rows=2, cols=1, shared_xaxes=False,
                     specs=[[{"type": "choropleth"}], [{"type": "table"}]],
                     shared_yaxes=False)
 fig.add_trace(
-    go.Choropleth(colorscale='Greens', locationmode='country names',
+    go.Choropleth(colorscale='BuGn', locationmode='country names',
                   locations=dff['Country'], marker_line_color='gray',
                   marker_opacity=0.75, marker_line_width=0.5, showscale=False,
                   z=dff['Share_2016'], hoverinfo='z+location'
-    ), row=1, col=1
+                  ), row=1, col=1
 )
 fig.add_trace(
     go.Table(
-        header=dict(values=df.columns[1:].tolist(), font=dict(size=10), align="center"),
+        header=dict(values=df.columns[1:].tolist(),
+                    font=dict(size=10), align="center"),
         cells=dict(
-            values=[df.query('date==2016')[k].tolist() for k in df.columns[1:]],
-            align = "center"
+            values=[df.query('date==2016')[k].tolist()
+                    for k in df.columns[1:]],
+            align="center"
         ),
     ),
     row=2, col=1
@@ -75,9 +78,9 @@ buttons = []
 for year in [2005, 2016]:
     dfp = df.query('date==@year').reset_index()
     buttons.append(
-        dict(method= 'update', label=str(year),
+        dict(method='update', label=str(year),
              args=[{"cells": dict(values=[df.query('date==@year')[k].tolist() for k in df.columns[1:]]),
-                    "z": [df.query('date==@year')['Share']] }])
+                    "z": [df.query('date==@year')['Share']]}])
     )
 
 fig.update_layout(updatemenus=[dict(active=1, buttons=buttons, bgcolor="#FFFFFF",
@@ -86,12 +89,13 @@ fig.update_layout(updatemenus=[dict(active=1, buttons=buttons, bgcolor="#FFFFFF"
                   geo=dict(
                       scope='europe', bgcolor="#F0F0F0", projection_scale=1.2,
                       center=dict(lat=60, lon=15)
-                    ),
-                  margin={"r": 0, "t": 20, "l": 0, "b": 0, "pad": 0, "autoexpand": True},
-                  paper_bgcolor = "#F0F0F0",
-                  height = 1200)
+),
+    margin={"r": 0, "t": 20, "l": 0, "b": 0,
+            "pad": 0, "autoexpand": True},
+    paper_bgcolor="#F0F0F0",
+    height=1200)
 
 fig.show(config=conf)
 
-fig.write_html("C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\vis\\packaging.html",
+fig.write_html("C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\pages\\packaging-graph.html",
                config=conf)
