983ed90d95dbb33a9aed7aa534f0494c487831fc
Ismael <ismael.em@outlook.com>
2020-09-14 18:49:17 +0300

    transport-graph: BuGn colors

--- vis/transport/transport-graph.py
+++ vis/transport/transport-graph.py
@@ -10,17 +10,19 @@ from plotly.subplots import make_subplots
 pd.set_option("display.max_columns", 40)
 pd.set_option("display.max_colwidth", 200)
 
-df = pd.read_csv("C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\vis\\transport\\change-energy-consumption.csv")
+df = pd.read_csv(
+    "C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\vis\\transport\\change-energy-consumption.csv")
 
 df.rename(columns={"geo": "Eurozone", "share": "Share"}, inplace=True)
-df["Transport mode"] = df["Transport mode"].str.replace("Final consumption - transport sector - ", "").str.title()
+df["Transport mode"] = df["Transport mode"].str.replace(
+    "Final consumption - transport sector - ", "").str.title()
 
 dff = df.pivot_table('obsValue', 'Eurozone', 'Transport mode').reset_index()
 
 # #############################################################################
 # variables
-EU13=["Bulgaria", "Czech Republic", "Cyprus", "Estonia", "Croatia", "Hungary",
-      "Lithuania", "Latvia", "Malta", "Poland", "Romania", "Slovenia", "Slovakia"]
+EU13 = ["Bulgaria", "Czech Republic", "Cyprus", "Estonia", "Croatia", "Hungary",
+        "Lithuania", "Latvia", "Malta", "Poland", "Romania", "Slovenia", "Slovakia"]
 EU15 = ["Austria", "Belgium", "Denmark", "Finland", "France", "Germany",
         "Greece", "Ireland", "Italy", "Luxembourg", "Netherlands", "Portugal",
         "Spain", "Sweden", "United Kingdom"]
@@ -44,40 +46,41 @@ fig = make_subplots(rows=2, cols=1, shared_xaxes=False,
                     shared_yaxes=False)
 
 fig.add_trace(
-    go.Choropleth(colorscale='Greens', hovertext=[],
-        locationmode='country names', locations=EEA33,
-        marker_line_color='gray', marker_opacity=0.75, marker_line_width=0.5,
-        showscale=False, z=[1]*35, hoverinfo='location'
-    ),
+    go.Choropleth(colorscale='BuGn', hovertext=[],
+                  locationmode='country names', locations=EEA33,
+                  marker_line_color='gray', marker_opacity=0.75, marker_line_width=0.5,
+                  showscale=False, z=[1]*35, hoverinfo='location'
+                  ),
     row=1, col=1
 )
 fig.add_trace(
     go.Table(
-        header=dict(values=dff.columns.tolist(), font=dict(size=10), align="center"),
+        header=dict(values=dff.columns.tolist(),
+                    font=dict(size=10), align="center"),
         cells=dict(
             values=[dff[k].tolist() for k in dff.columns],
-            align = "center", fill=dict(color=[["#90ce96", "#e5ecf6", "#e5ecf6"]])
+            align="center", fill=dict(color=[["#86CDB9", "#e5ecf6", "#e5ecf6"]])
         ),
     ),
     row=2, col=1
 )
 
 
-button_EEA33  = dict(method= 'update', label='EEA-33',
-                     args=[{"locations": [EEA33],
-                            "cells":  dict(
-                                values=[dff[k].tolist() for k in dff.columns],
-                                fill=dict(color=[["#90ce96", "#e5ecf6", "#e5ecf6"]]) )}])
-button_EU13  = dict(method= 'update', label='EU-13',
-                    args=[{"locations": [EU13],
+button_EEA33 = dict(method='update', label='EEA-33',
+                    args=[{"locations": [EEA33],
                            "cells":  dict(
-                               values=[dff[k].tolist() for k in dff.columns],
-                               fill=dict(color=[["#e5ecf6", "#90ce96", "#e5ecf6"]]) )}])
-button_EU15  = dict(method= 'update', label='EU-15',
-                    args=[{"locations": [EU15],
-                           "cells":  dict(
-                               values=[dff[k].tolist() for k in dff.columns],
-                               fill=dict(color=[["#e5ecf6", "#e5ecf6", "#90ce96"]]) )}])
+                        values=[dff[k].tolist() for k in dff.columns],
+                        fill=dict(color=[["#86CDB9", "#e5ecf6", "#e5ecf6"]]))}])
+button_EU13 = dict(method='update', label='EU-13',
+                   args=[{"locations": [EU13],
+                          "cells":  dict(
+                       values=[dff[k].tolist() for k in dff.columns],
+                       fill=dict(color=[["#e5ecf6", "#86CDB9", "#e5ecf6"]]))}])
+button_EU15 = dict(method='update', label='EU-15',
+                   args=[{"locations": [EU15],
+                          "cells":  dict(
+                       values=[dff[k].tolist() for k in dff.columns],
+                       fill=dict(color=[["#e5ecf6", "#e5ecf6", "#86CDB9"]]))}])
 buttons = [button_EEA33, button_EU13, button_EU15]
 fig.update_layout(updatemenus=[dict(active=0, buttons=buttons, bgcolor="#FFFFFF",
                                     x=0.03, xanchor="left", yanchor="top")],
@@ -85,11 +88,13 @@ fig.update_layout(updatemenus=[dict(active=0, buttons=buttons, bgcolor="#FFFFFF"
                   geo=dict(
                       scope='europe', bgcolor="#F0F0F0", projection_scale=1.2,
                       center=dict(lat=60, lon=15)
-                    ),
-                  margin={"r": 0, "t": 20, "l": 0, "b": 0, "pad": 0, "autoexpand": True},
-                  paper_bgcolor = "#F0F0F0",
-                  height = 700)
+),
+    margin={"r": 0, "t": 20, "l": 0, "b": 0,
+            "pad": 0, "autoexpand": True},
+    paper_bgcolor="#F0F0F0",
+    height=700)
 
 fig.show(config=conf)
-fig.write_html("C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\vis\\transport.html",
+
+fig.write_html("C:\\Users\\ismae\\Documents\\GitHub\\dataseeds.github.io\\pages\\transport-graph.html",
                config=conf)
